afc6f098008f9d80f3afa44b913feb2611ad93fb
Jeff Strom <jeff@pulsemedica.com>
2023-03-09 16:02:53 -0700

    SIF-272: - Change 'moveFile' function to only move .hdf5 files (since the .db files are likely to not need to be moved as urgently or will have a different impl) - Make test idempotent (all calls to it produce the same predictable output and do not require or create lingering files or folders)

--- Filesystem.py
+++ Filesystem.py
@@ -1,6 +1,42 @@
 import hashlib
 import os
+from pathlib import Path
+import random
 import shutil
+import string
+
+# Returns a random string of mixed uppper/lower case chars of N length.
+def get_random_string(length):
+    letters = string.ascii_letters
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str;
+
+# Creates a subfolder named with random chars to prevent collision with existing folder.
+def createRandomSubfolder(startingDir, random_name_length):
+    randomizedTestSubfolderName = get_random_string(random_name_length);
+    newpath = os.path.join(startingDir, randomizedTestSubfolderName);
+    if not os.path.exists(newpath):
+        os.makedirs(newpath);
+    return newpath;
+
+# Removes an existing directory. NOTE: the directory must be empty.
+def removeDirectory(dir):
+    try:
+        os.rmdir(dir);
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (dir, e))
+        
+# Removes any existing files from the given directory.
+def removeAllFilesFromFolder(dir):
+    for filename in os.listdir(dir):
+        file_path = os.path.join(dir, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 # Removes all '\\' chars from a file path string that might cause errors.
 def convertWindowsToUnixFilepath(windowsFilepath):
@@ -21,27 +57,35 @@ def countFilesPerFolder(dir_path):
             count += 1;
     return count;
 
-# Moves all .db or .hdf5 files from one directory to another, ensuring their sha256 hashes match before/after moving them.
-def moveDirectoryFiles(directoryString, destinationFolder):
-    if(countFilesPerFolder(directoryString) == 0):
+def getFirstFileInDirectory(dir_path):
+    for path in os.listdir(dir_path):
+        if os.path.isfile(os.path.join(dir_path, path)):
+            return path;
+    return "";
+
+# If there is more than one hdf5 file in the current directory, this function moves all .hdf5 files (other than the most recent one) from one directory to another, 
+# ensuring their sha256 hashes match before/after moving them.
+def moveDirectoryHDF5Files(directoryString, destinationFolder):
+    if(countFilesPerFolder(directoryString)==0 or (countFilesPerFolder(directoryString)==1 and getFirstFileInDirectory(directoryString).find(".hdf5") != -1)):
         print("No files to move! Ending early.");
         return;
-    directory = os.fsencode(directoryString);
     filesToMove=[];
-    print(directory);
-    byteString = os.listdir(directory);
-    for file in byteString:
+    print(directoryString);
+    paths = sorted(Path(directoryString).iterdir(), key=os.path.getmtime); #get files in directory listed by last date modified from oldest to newest
+    print(paths);
+    paths.pop(); #remove the oldest recent file
+    print(paths);
+    for file in paths:
          filename = os.fsdecode(file)
-         if filename.endswith(".db") or filename.endswith(".hdf5"):
-             startingFilepath = os.path.join(directory.decode("utf-8"), filename);
-             destinationFilepath = os.path.join(destinationFolder, filename);
+         if filename.endswith(".hdf5"):
+             startingFilepath = filename;
+             destinationFilepath = os.path.join(destinationFolder, os.path.basename(filename));
              originalHash = getFileHash(startingFilepath)
              shutil.move(startingFilepath,destinationFilepath);
              if(getFileHash(destinationFilepath)!=originalHash):
                 print("ERROR! File checksums do not match");
              else:
                 print("File successfully moved: " + destinationFilepath);
-             continue
          else:
              continue
     return filesToMove;
--- tests.py
+++ tests.py
@@ -1,19 +1,55 @@
 import Filesystem;
-import sys;
+import os;
+import time;
 
-def moveFileTest(args):
-    Filesystem.moveDirectoryFiles(Filesystem.convertWindowsToUnixFilepath(args[0]), Filesystem.convertWindowsToUnixFilepath(args[1]));
+# Test of 'Filesystem.moveDirectoryHDF5Files()' function. Will return boolean True if tests passes, else False.
+def moveFileTest():
+
+    # Create dummy files/folders.
+    testFileRandomChars = 30;
+    testStartDir = Filesystem.createRandomSubfolder(os.getcwd(), testFileRandomChars);
+    testEndDir = Filesystem.createRandomSubfolder(os.getcwd(), testFileRandomChars);
+
+    print(testStartDir);
+    print(testEndDir);
+    with open(os.path.join(testStartDir, "patient1.hdf5"), 'w') as fp:
+        fp.write('test data1');
+        fp.close();
+    time.sleep(1.5); # pause between file creation so one file is noticably older than another.
+    with open(os.path.join(testStartDir, "patient2.hdf5"), 'w') as fp:
+        fp.write('test data2');
+        fp.close();
+    if(Filesystem.countFilesPerFolder(testStartDir) != 2):
+        print('ERROR! Could not create necessary test files.');
+        return False;
+
+    # Run test.
+    Filesystem.moveDirectoryHDF5Files(Filesystem.convertWindowsToUnixFilepath(testStartDir), Filesystem.convertWindowsToUnixFilepath(testEndDir));
+    if(Filesystem.countFilesPerFolder(testStartDir) != 1 or Filesystem.countFilesPerFolder(testEndDir) != 1):
+        print('ERROR! Could not successfully move test files.');
+        return False;
+
+    # Cleanup.
+    Filesystem.removeAllFilesFromFolder(testStartDir);
+    Filesystem.removeAllFilesFromFolder(testEndDir);
+
+    if(Filesystem.countFilesPerFolder(testStartDir) != 0 or Filesystem.countFilesPerFolder(testEndDir) != 0):
+        print('ERROR! Could not successfully remove test files.');
+        return False;
+
+    Filesystem.removeDirectory(testStartDir);
+    Filesystem.removeDirectory(testEndDir);
+
+    if(os.path.exists(testStartDir) or os.path.exists(testEndDir)):
+        print("ERROR! Could not cleanup test directories properly");
+        return False;
+
     return True;
 
 
 # MAIN=========
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    if (len(args)<2):
-        print("ERROR! Need input/output folder as args to move files");
-        exit(-1);
-
-    if(moveFileTest(args) == False):
+    if(moveFileTest() == False):
         print("moveFileTest FAILED");
         exit(-2);
     else:
